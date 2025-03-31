3e060dd661db588859a0f88500c5eb32ed97a21b
sweber <h.khayami@think-modular.com>
2025-03-31 15:23:18 +0200

    Up version

--- version.php
+++ version.php
@@ -32,8 +32,8 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
 
 // Version.
-$plugin->version = 2025033101;
-$plugin->release = '1.2.10';
+$plugin->version = 2025033102;
+$plugin->release = '1.2.11';
 
 // Supported versions.
 $plugin->supported = [404, 405];
